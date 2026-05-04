65fdb5598150c7e71939b2240f2bee3aebb4b9ae
imakshat47 <imakshat47@gmail.com>
2026-05-04 20:30:37 +0530

    Update from Lappy by ImAkshat47 - utils.py at 2026-05-04_20-29-53_+05:30

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def ensure_list(x):
     """
     Normalize openEHR structures where 'items' or 'description.items'
     may be either a dict or a list. This is required because your attached
-    composition files contain both variants. [1](https://stmicroelectronics-my.sharepoint.com/personal/akshat_kumar03_st_com/Documents/Microsoft%20Copilot%20Chat%20Files/1.2.2.489752100565248525010.049995050100494850555.510157981014898535356.559949102494855495598.101102481005156554948.json)[1](https://stmicroelectronics-my.sharepoint.com/personal/akshat_kumar03_st_com/Documents/Microsoft%20Copilot%20Chat%20Files/1.2.2.489752100565248525010.049995050100494850555.510157981014898535356.559949102494855495598.101102481005156554948.json)[2](https://stmicroelectronics-my.sharepoint.com/personal/akshat_kumar03_st_com/Documents/Microsoft%20Copilot%20Chat%20Files/1.2.1.48545410099491024998.10250574910157545151.97100574853569797505.44849575598561015210.156985356102495398559857.json)
+    composition files contain both variants. [1]
     """
     if x is None:
         return []
